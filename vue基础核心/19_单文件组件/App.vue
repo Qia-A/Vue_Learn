49544bfd29df1218d45e74f8ddc2c4c189982b8a
Qia-A <98876342+Qia-A@users.noreply.github.com>
2022-12-25 18:04:37 +0800

    Vue

---
+++ vue基础核心/19_单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<School></School>
+		<Stundent></Stundent>
+	</div>
+</template>
+
+<script>
+	//引入组件
+	import School from './School.vue'
+	import Stundent from './Student.vue'
+
+	export default {
+		name:'App',
+		components:{
+			School,
+			Stundent
+		}
+	}
+</script>
+
+<style>
+
+</style>
